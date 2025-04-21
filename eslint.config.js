130d86584bbec67e96edbef9dcc028981e8c180a
Yut8853 <hello@junkbranding.com>
2025-04-21 19:03:36 +0900

    fix husky

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+// eslint.config.js
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import typescript from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+
+export default [
+    js.configs.recommended,
+    {
+        files: ['**/*.ts', '**/*.tsx'],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+        plugins: {
+            '@typescript-eslint': typescript,
+            react,
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+        rules: {
+            semi: ['error', 'always'],
+            quotes: ['error', 'single'],
+            '@typescript-eslint/no-unused-vars': 'warn',
+            'react/react-in-jsx-scope': 'off',
+            'react/prop-types': 'off',
+            'prettier/prettier': 'error',
+        },
+    },
+    prettier,
+];
